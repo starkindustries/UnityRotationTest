9575501d4b144cb55c55c99b829b4e7af980a8ec
Zion Perez <zionperez@gmail.com>
2019-07-02 10:16:25 +0900

    Simplified the project; removed the buttons and button code. To test, set the transform's rotation y to 0 and press play; ship spins slow. Then set y to 180 and press play; ship spins insanely fast.

--- Assets/RotationTest.cs
+++ Assets/RotationTest.cs
@@ -1,27 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RotationTest : MonoBehaviour
 {    
-    // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TestRotation sequence initiated!");
         Debug.Log("Current rotation: " + transform.rotation.ToString());              
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.angularVelocity = 10f;
-    }
-
-    public void DidPressEulerButton()
-    {
-        transform.rotation = Quaternion.Euler(x: 0f, y: 180f, z: 0f);
-        Debug.Log("Quaternion.Euler(x: 0f, y: 180f, z: 0f): " + transform.rotation.ToString());
-    }
-
-    public void DidPressQuaternionButton()
-    {
-        transform.rotation = new Quaternion(x: 0f, y: 1f, z: 0f, w: 0f);
-        Debug.Log("Quaternion(x: 0f, y: 1f, z: 0f, w: 0f): " + transform.rotation.ToString());
     }
 }
